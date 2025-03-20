f9d6508c192b7ce3eb52d1d666ad80c813852a4a
Hamraz Hakeem <hamrazhakeem100@gmail.com>
2025-03-20 21:28:39 +0530

    feat: Add YouTube and X profile link to social media icons in intro section

--- components/intro.tsx
+++ components/intro.tsx
@@ -98,7 +98,7 @@ const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
 
         <div className="flex gap-2">
           <a
-            className="bg-white p-4 text-gray-700 hover:text-blue-700 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+            className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
             href="https://www.linkedin.com/in/hamrazhakeem"
             target="_blank"
           >
@@ -122,7 +122,7 @@ const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
           </a>
 
           <a
-            className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-red-600 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+            className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
             href="https://www.youtube.com/@hamrazhakeem"
             target="_blank"
           >
