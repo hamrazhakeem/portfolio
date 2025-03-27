a92561ab7305c0dc5c5f933f8e9f1043931e213a
Hamraz Hakeem <hamrazhakeem100@gmail.com>
2025-03-27 12:31:36 +0530

    feat: add Simple analytics tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,6 +48,13 @@ export default function RootLayout({
             })(window, document, "clarity", "script", "qun7bkfmmn");
           `}
         </Script>
+        
+        {/* Simple Analytics - 100% privacy-first analytics */}
+        <Script 
+          src="https://scripts.simpleanalyticscdn.com/latest.js" 
+          strategy="afterInteractive"
+          data-collect-dnt="true"
+        />
       <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
       <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
         <ThemeContextProvider>
