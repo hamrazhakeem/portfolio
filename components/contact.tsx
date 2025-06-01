37859a4fcbc7ce28589c8463e2aad9aa968491af
Hamraz Hakeem <hamrazhakeem100@gmail.com>
2025-06-01 20:53:13 +0530

    docs: update README to reflect current service downtime status

--- components/contact.tsx
+++ components/contact.tsx
@@ -31,14 +31,6 @@ export default function Contact() {
     >
       <SectionHeading>Contact me</SectionHeading>
 
-    <p className="text-gray-700 -mt-6 dark:text-white/80">
-        Please contact me directly at{" "}
-        <a className="underline" href="mailto:hamrazhakeem100@gmail.com">
-            hamrazhakeem100@gmail.com
-        </a>{" "}
-        or through this form.
-    </p>
-
       <form
         className="mt-10 flex flex-col dark:text-black"
         action={async (formData) => {
